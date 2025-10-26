eaa1df4239a2c408c1cc47e96a1c0eb839a8811d
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 17:54:54 -0700

    Update arman.js

--- characters/arman/arman.js
+++ characters/arman/arman.js
@@ -1,1 +1,20 @@
-// Arman logic coming soon
+export default {
+  name: "Arman",
+  maxHP: 50,
+  currentHP: 50,
+  attacks: [
+    {
+      name: "Weekends Fortnite",
+      description: "2/7 chance to deal 50 damage to any enemy and doubles teammates' damage next turn if alive; 5/7 chance to get grounded (can't use this attack next round)"
+    },
+    {
+      name: "The Art Of Hurting",
+      description: "Deal 20 damage to any enemy"
+    },
+    {
+      name: "The Art Of Healing",
+      description: "Heal 20 HP to any teammate or enemy (including yourself)"
+    }
+  ],
+  passive: [],
+};
