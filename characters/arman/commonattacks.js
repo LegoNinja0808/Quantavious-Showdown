e69332cd58d32bfa2d53954f27b819f7eb4bb7e0
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 20:57:50 -0700

    Update commonattacks.js

--- characters/arman/commonattacks.js
+++ characters/arman/commonattacks.js
@@ -1,14 +1,38 @@
 export default [
   {
     name: "Weekends Fortnite",
-    description: "2/7 chance to deal 50 damage to any enemy and make teammates' damage x2 next turn. 5/7 chance to get grounded; can't use this attack for 1 round."
+    type: "targeted",
+    damage: 50,
+    chance: 2/7,
+    effectChance: 5/7,
+    resolve: function(target, self, teammates) {
+      if (Math.random() < this.chance) {
+        target.currentHP -= this.damage;
+        teammates.forEach(t => t.nextDamageMultiplier = 2);
+        return `${self.name} hits ${target.name} for ${this.damage} damage! Teammates will do double damage next turn!`;
+      } else {
+        self.grounded = true;
+        return `${self.name} got grounded! Can't use this attack next round.`;
+      }
+    }
   },
   {
     name: "The Art Of Hurting",
-    description: "Deals 20 damage to any enemy."
+    type: "targeted",
+    damage: 20,
+    resolve: function(target) {
+      target.currentHP -= this.damage;
+      return `${target.name} takes ${this.damage} damage from The Art Of Hurting!`;
+    }
   },
   {
     name: "The Art Of Healing",
-    description: "Heal 20 HP to any teammate or enemy (including yourself)."
+    type: "targeted",
+    heal: 20,
+    resolve: function(target) {
+      target.currentHP += this.heal;
+      if (target.currentHP > target.maxHP) target.currentHP = target.maxHP;
+      return `${target.name} is healed for ${this.heal} HP!`;
+    }
   }
 ];
