3ba4168f2f5f02d7b3e15f3eb43a804af58abc4a
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 20:57:27 -0700

    Update commonattacks.js

--- characters/micah/commonattacks.js
+++ characters/micah/commonattacks.js
@@ -1,10 +1,26 @@
 export default [
   {
     name: "Jump",
-    description: "Deals 20 damage to any enemy and 25% chance to stun the enemy, skipping their next turn (Earthquake Effect)."
+    type: "targeted",
+    damage: 20,
+    stunChance: 0.25,
+    resolve: function(target) {
+      target.currentHP -= this.damage;
+      let msg = `${this.name} hits ${target.name} for ${this.damage} damage!`;
+      if (Math.random() < this.stunChance) {
+        target.stunned = true;
+        msg += ` ${target.name} is stunned and will skip their next turn!`;
+      }
+      return msg;
+    }
   },
   {
     name: "Shockwave",
-    description: "50% incoming damage to one character on your team (not yourself) for one turn."
+    type: "targeted",
+    damageReduction: 0.5, // 50% incoming
+    resolve: function(target) {
+      target.damageReduction = this.damageReduction;
+      return `${target.name} will take 50% less damage from the next attack!`;
+    }
   }
 ];
