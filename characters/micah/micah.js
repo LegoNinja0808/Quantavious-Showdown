9d1c44cf51912e8212b57b424e7ed579e0d00e2c
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 17:54:07 -0700

    Update micah.js

--- characters/micah/micah.js
+++ characters/micah/micah.js
@@ -1,1 +1,16 @@
-// Micah logic coming soon
+export default {
+  name: "Micah",
+  maxHP: 60,
+  currentHP: 60,
+  attacks: [
+    {
+      name: "Jump",
+      description: "Deals 20 Damage To Any Enemy & 25% Chance To Stun The Enemy (skips their next turn)"
+    },
+    {
+      name: "Shockwave",
+      description: "50% Incoming Damage To One Character On Your Team (not yourself) for one turn"
+    }
+  ],
+  passive: [],
+};
