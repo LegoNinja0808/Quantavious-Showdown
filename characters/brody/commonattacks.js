b036ba3f6bfa9d8f946a901c0ddd2788cd573d95
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 20:58:17 -0700

    Update commonattacks.js

--- characters/brody/commonattacks.js
+++ characters/brody/commonattacks.js
@@ -1,10 +1,32 @@
 export default [
   {
     name: "Drama Kid",
-    description: "Scares everyone except Johan, not allowing them to attack or defend next turn (still allows healing)."
+    type: "aoe", // affects multiple enemies
+    resolve: function(targets) {
+      let messages = [];
+      targets.forEach(t => {
+        if (t.name !== "Johan") {
+          t.cannotAttack = true;
+          t.cannotDefend = true;
+          messages.push(`${t.name} is scared and cannot attack or defend next turn!`);
+        }
+      });
+      return messages.join(' ');
+    }
   },
   {
     name: "Lifesteal",
-    description: "Get a random number 1-40. Deal that much damage to any character and heal yourself by the same amount."
+    type: "targeted",
+    resolve: function(target, self) {
+      const dmg = Math.floor(Math.random() * 40) + 1;
+      target.currentHP -= dmg;
+      self.currentHP += dmg;
+      let msg = `${self.name} deals ${dmg} damage to ${target.name} and heals for ${dmg} HP!`;
+      if (self.currentHP > 100) {
+        self.currentHP = 0;
+        msg += ` Oh no! ${self.name} overhealed above 100 HP and dies!`;
+      }
+      return msg;
+    }
   }
 ];
