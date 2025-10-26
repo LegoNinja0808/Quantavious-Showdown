9fd2a867469d89dfef0a5bc73a5d73d9e6cb1bf0
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 20:50:55 -0700

    Add Jump and Shockwave attacks for Micah
    Added new attack options for Micah with descriptions.

--- characters/micah/commonattacks.js
+++ characters/micah/commonattacks.js
@@ -1,1 +1,10 @@
-# Wait
+export default [
+  {
+    name: "Jump",
+    description: "Deals 20 damage to any enemy and 25% chance to stun the enemy, skipping their next turn (Earthquake Effect)."
+  },
+  {
+    name: "Shockwave",
+    description: "50% incoming damage to one character on your team (not yourself) for one turn."
+  }
+];
