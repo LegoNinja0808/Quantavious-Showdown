75a57b39c6e74b07b4d9a8cab46e3ffe1fe5341d
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 20:51:36 -0700

    Add common attack definitions for characters

--- characters/arman/commonattacks.js
+++ characters/arman/commonattacks.js
@@ -1,1 +1,14 @@
-# Wait
+export default [
+  {
+    name: "Weekends Fortnite",
+    description: "2/7 chance to deal 50 damage to any enemy and make teammates' damage x2 next turn. 5/7 chance to get grounded; can't use this attack for 1 round."
+  },
+  {
+    name: "The Art Of Hurting",
+    description: "Deals 20 damage to any enemy."
+  },
+  {
+    name: "The Art Of Healing",
+    description: "Heal 20 HP to any teammate or enemy (including yourself)."
+  }
+];
