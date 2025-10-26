b5eabfc4deb942ebc06da760fb2dc80c5a75a7af
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 20:52:17 -0700

    Add common attack moves for character Jonathan

--- characters/jonathan/commonattacks.js
+++ characters/jonathan/commonattacks.js
@@ -1,1 +1,10 @@
-# Wait
+export default [
+  {
+    name: "Chomp & Crunch",
+    description: "Deals 10 damage per character on the enemy's team to one enemy."
+  },
+  {
+    name: "Block Next Attack",
+    description: "Eats the next attack and takes no damage (can only be used once per game)."
+  }
+];
