0f3af03cf55e4b18042bfd16ea76f3bbf00b8e80
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 17:52:25 -0700

    Implement Johan character logic and attacks

--- characters/johan/johan.js
+++ characters/johan/johan.js
@@ -1,1 +1,16 @@
-// Johan logic coming soon
+export default {
+  name: "Johan",
+  maxHP: 40,
+  currentHP: 40,
+  attacks: [
+    {
+      name: "Eat A Taco?",
+      description: "50% Chance To Deal 50 Damage To Any Enemy, 50% Chance To Deal 39 Damage To Self. Then Activate Sacrificial Pawn If Still Alive"
+    },
+    {
+      name: "Sacrificial Pawn",
+      description: "Takes the next hit, then dies"
+    }
+  ],
+  passive: [],
+};
