b64178091544ab37b78dc09a64dc03e7e9b03814
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 17:55:21 -0700

    Implement Brody character with abilities
    Added Brody character with attributes and attacks.

--- characters/brody/brody.js
+++ characters/brody/brody.js
@@ -1,1 +1,21 @@
-// Brody logic coming soon
+export default {
+  name: "Brody",
+  maxHP: 70,
+  currentHP: 70,
+  attacks: [
+    {
+      name: "Drama Kid",
+      description: "Scares everyone but Johan, not allowing them to attack or defend next turn (healing still works)"
+    },
+    {
+      name: "Lifesteal",
+      description: "Get a random number 1-40; deal that much damage to any character and heal yourself by the same amount"
+    }
+  ],
+  passive: [
+    {
+      name: "Over Heal",
+      description: "Starts at 70 HP. Can heal up to 100HP. If healed above 100HP (101+), instantly dies."
+    }
+  ],
+};
