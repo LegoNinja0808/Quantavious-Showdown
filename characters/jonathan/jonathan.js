95ed91c01cf42e7efb2b0052241aedce1e1f4a2f
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 17:55:59 -0700

    Update jonathan.js

--- characters/jonathan/jonathan.js
+++ characters/jonathan/jonathan.js
@@ -1,1 +1,16 @@
-// Jonathan logic coming soon
+export default {
+  name: "Jonathan",
+  maxHP: 80,
+  currentHP: 80,
+  attacks: [
+    {
+      name: "Chomp & Crunch",
+      description: "Deals 10 damage per character on the enemy's team to one enemy"
+    },
+    {
+      name: "Block Next Attack",
+      description: "Eats the next attack and takes no damage (can only be used once per game)"
+    }
+  ],
+  passive: [],
+};
