45f504df8da307b7431fccab5e13338c714c6f1b
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 20:50:38 -0700

    Add new common attacks for Johan
    Added new attack options for Johan, including 'Eat A Taco?' and 'Sacrificial Pawn'.

--- characters/johan/commonattacks.js
+++ characters/johan/commonattacks.js
@@ -1,1 +1,10 @@
-# Wait
+export default [
+  {
+    name: "Eat A Taco?",
+    description: "50% chance to deal 50 damage to any enemy, 50% chance to deal 39 damage to self. Then activate Sacrificial Pawn if still alive."
+  },
+  {
+    name: "Sacrificial Pawn",
+    description: "Johan takes the next hit, and then he dies."
+  }
+];
