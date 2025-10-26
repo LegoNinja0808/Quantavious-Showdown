66ba0197ff204f20f86c8d2bdd4e1689fc04d253
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 20:57:03 -0700

    Refactor common attacks for Johan character

--- characters/johan/commonattacks.js
+++ characters/johan/commonattacks.js
@@ -1,10 +1,30 @@
 export default [
   {
     name: "Eat A Taco?",
-    description: "50% chance to deal 50 damage to any enemy, 50% chance to deal 39 damage to self. Then activate Sacrificial Pawn if still alive."
+    type: "targeted", // player chooses target
+    damage: 50,
+    selfDamage: 39,
+    chance: 0.5, // 50% chance to hit enemy / self
+    effect: "If Johan survives, can use Sacrificial Pawn next",
+    resolve: function(target, self) {
+      if (Math.random() < 0.5) {
+        target.currentHP -= this.damage;
+        return `${self.name} successfully hits ${target.name} for ${this.damage} damage!`;
+      } else {
+        self.currentHP -= this.selfDamage;
+        return `${self.name} accidentally hurts themselves for ${this.selfDamage} damage!`;
+      }
+    }
   },
   {
     name: "Sacrificial Pawn",
-    description: "Johan takes the next hit, and then he dies."
+    type: "passive/triggered",
+    description: "Johan takes the next hit, then dies",
+    resolve: function(attacker, self) {
+      const dmg = attacker.currentDamage || 0;
+      self.currentHP = 0;
+      attacker.currentHP -= 0; // attacker still takes normal attack? depends
+      return `${self.name} took the hit and dies!`;
+    }
   }
 ];
