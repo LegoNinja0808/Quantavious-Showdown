b4a57753b3fba0a358fcff281b417e0e0af9151f
LegoNinja0808 <martinezmicah209@gmail.com>
2025-10-25 20:51:56 -0700

    Add common attacks for Brody character

--- characters/brody/commonattacks.js
+++ characters/brody/commonattacks.js
@@ -1,1 +1,10 @@
-# Wait
+export default [
+  {
+    name: "Drama Kid",
+    description: "Scares everyone except Johan, not allowing them to attack or defend next turn (still allows healing)."
+  },
+  {
+    name: "Lifesteal",
+    description: "Get a random number 1-40. Deal that much damage to any character and heal yourself by the same amount."
+  }
+];
